1d3454a2a797b910683d486d25550f95c1ce3da6
Jan Weidner <jw3126@gmail.com>
2021-09-22 08:38:45 +0200

    fix CUDA tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,17 @@
 include("test_capi.jl")
 include("test_highlevel.jl")
+
+CUDA_INSTALLED = false
+CUDA_FUNCTIONAL = false
 try
     import CUDA
     global CUDA_INSTALLED = true
+    global CUDA_FUNCTIONAL = CUDA.functional()
 catch
-    global CUDA_INSTALLED = false
 end
-CUDA_FUNCTIONAL = CUDA.functional()
 if CUDA_INSTALLED && CUDA_FUNCTIONAL
     @info """
-    Found a working CUDA.jl package, running GPU tests
+    Found a working CUDA.jl package, running CUDA tests
     """
     include("test_cuda.jl")
 else
